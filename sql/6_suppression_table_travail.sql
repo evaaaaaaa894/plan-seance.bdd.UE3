23cc59ea9a2380bd49a98bbd972aedbf7fdacb8b
eva <eva24riviere@icloud.com>
2026-02-07 21:12:34 +0100

    add half script6

---
+++ sql/6_suppression_table_travail.sql
@@ -0,0 +1,23 @@
+BEGIN;
+
+-- nom de schéma définitif
+SET search_path TO psch;
+
+
+
+--tables à supp : temporaire
+
+DROP table TMP_CNC ;
+
+DROP table TMP_etab_cine ;
+
+DROP table TMP_programmation ;
+
+DROP table TMP_rsa ;
+
+DROP table TMP_wiki1 ;
+
+DROP table TMP_wiki2 ;
+
+
+commit ;
